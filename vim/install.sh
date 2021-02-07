7883037fdeb1078dca326689d34358385f51314e
Igor <svilenkov@users.noreply.github.com>
2021-02-07 17:36:18 +0100

    fixes PlugInstall stderr

--- vim/install.sh
+++ vim/install.sh
@@ -12,4 +12,4 @@ fi
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-vim +PluginInstall +qall
+vim +'PlugInstall --sync' +qa
